e3b5e0a6a21f80f42cc3c6cd1edbe58e2fb95bc5
yagosantosog <santosyago2005@gmail.com>
2024-02-28 09:40:34 -0300

    adjust mobile menu

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,28 @@
 const menu = document.getElementById('menu-mobile');
-const close_btn = document.getElementById('close-btn');
 let navbar = document.getElementById('navbar-mobile');
-menu.onclick = function abrirMenu() {
-    if(navbar.style.display == 'flex'){
-        navbar.style.display = 'none'
-    }else{
-        navbar.style.display = 'flex';
-    }
-    
+
+function animation(url, display) {
+    menu.classList.add('rotate-scale-up');
+    menu.addEventListener('animationend', () => {
+        menu.classList.remove('rotate-scale-up');
+        navbar.style.display = display;
+        if(display != "none") {
+            navbar.classList.add("scale-up-center");
+            navbar.addEventListener('animationend', () => {
+                navbar.classList.remove("scale-up-center")
+                menu.querySelector('img').setAttribute('src', url);
+            })
+        }else {
+            menu.querySelector('img').setAttribute('src', url);
+        }
+    })
 }
 
-close_btn.onclick = function fecharMeny() {
-    navbar.style.display = 'none'
+menu.onclick = function abrirMenu() {
+    if(navbar.style.display == 'flex'){
+        animation("./img/menu.svg", "none",);
+    }else{
+        animation("./img/close.svg", "flex");
+    }
+    
 }
